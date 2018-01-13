b33706d4bcf2ee73974f7f1c542bfb7f83b79896
Ibrakov Azat <azatibrakov@gmail.com>
2018-01-13 17:22:45 +0700

    Bump version: 0.0.5 → 0.0.6

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,4 +3,4 @@ Script for generating list of files paths in hierarchical order
 for correct "SQL"-files compilation.
 """
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
--- scripts/seek-well.py
+++ scripts/seek-well.py
@@ -34,7 +34,7 @@ from sqlparse.sql import (Token,
 from sqlparse.tokens import (Keyword,
                              Punctuation)
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 logger = logging.getLogger(__name__)
 
