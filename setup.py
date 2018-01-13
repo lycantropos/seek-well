24b752dc294815443e6e7dbb29c81b0cf78837ef
Ibrakov Azat <azatibrakov@gmail.com>
2018-01-13 16:31:27 +0700

    Completed meta-info

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
 
+project_base_url = 'https://github.com/lycantropos/seek-well/'
 setup(name='seek-well',
       version='0.0.5',
       scripts=['seek-well.py'],
       description='Script for generating list of files paths '
                   'in hierarchical order '
                   'for correct "SQL"-files compilation.',
+      long_description=open('README.rst').read(),
       author='Azat Ibrakov',
       author_email='azatibrakov@gmail.com',
-      url='https://github.com/lycantropos/seek-well',
-      download_url='https://github.com/lycantropos/seek-well/archive/'
-                   'master.tar.gz',
+      url=project_base_url,
+      download_url=project_base_url + 'archive/master.zip',
       keywords=['sql'],
       install_requires=[
           'sqlparse>=0.2.4',  # parsing SQL statements
